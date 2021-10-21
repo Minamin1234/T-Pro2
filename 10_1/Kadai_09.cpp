a6370148c65059c439de1bc4523a2d45f57dcfa6
Minamin <62072288+Minamin1234@users.noreply.github.com>
2021-10-21 11:43:18 +0900

    Added old project.

---
+++ 10_1/Kadai_09.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    string str = "ABCDE";
+    for (int i = 0; i < str.size(); i++)
+    {
+        cout << str[i];
+    }
+    return 0;
+}
