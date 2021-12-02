673873911ebb9e49f9e3b628a070885cd0b8be2d
Minamin <62072288+Minamin1234@users.noreply.github.com>
2021-12-03 00:55:09 +0900

    Creating kadai code.

---
+++ 12_3/ht21a099Kadais.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+
+using namespace std;
+
+const int x_size = 20;
+const int y_size = 10;
+
+void set0(int a[y_size][x_size])
+{
+    for (int i = 0; i < x_size; i++)
+    {
+        for (int ii = 0; ii < y_size; i++)
+        {
+            a[ii][i] = 0;
+        }
+    }
+}
+
+void disparray(int a[y_size][x_size])
+{
+    for (int i = 0; i < y_size; i++)
+    {
+        for (int ii = 0; ii < x_size; ii++)
+        {
+            cout << a[ii][i] << ",";
+        }
+    }
+    cout << endl;
+}
+
+void setk(int a[y_size][x_size],int k)
+{
+    for (int i = 0; i < y_size; i++)
+    {
+        for (int ii = 0; ii < x_size; ii++)
+        {
+            a[ii][i] = k;
+        }
+    }
+}
+
+int sumof(int a[y_size][x_size])
+{
+    for(int i)
+}
+void sumof(int a[])
+int main()
+{
+    int b[]
+    return 0;
+}
